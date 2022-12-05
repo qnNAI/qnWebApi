a2e4408cdc887565f606de7786fa78708eaf0c6b
Руслан Ключенко <klyuchenkorus@gmail.com>
2022-12-05 14:17:58 +0300

    feat: added controller methods for product crud

--- WebApplication2/Controllers/ProductController.cs
+++ WebApplication2/Controllers/ProductController.cs
@@ -20,7 +20,7 @@ namespace Api.Controllers {
             return Ok(products);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:guid}")]
         public async Task<IActionResult> GetById([FromRoute] Guid id) {
             var product = await _service.GetByIdAsync(id);
             if (product is null) {
@@ -34,6 +34,24 @@ namespace Api.Controllers {
         public async Task<IActionResult> Add([FromBody] CreateProductRequest request) {
             var created = await _service.AddAsync(request);
             return CreatedAtAction("GetById", new { id = created.Id }, created);
+        }
+
+        [HttpPatch("{id:guid}")]
+        [Authorize]
+        public async Task<IActionResult> Update([FromBody] ProductDto product, [FromRoute] Guid id) {
+            if (product.Id != id) {
+                return BadRequest("Invalid id");
+            }
+
+            var updated = await _service.UpdateAsync(product);
+            return Ok(updated);
+        }
+
+        [HttpDelete("{id:guid}")]
+        [Authorize]
+        public async Task<IActionResult> Delete([FromRoute] Guid id) {
+            await _service.DeleteAsync(id);
+            return NoContent();
         }
     }
 }
